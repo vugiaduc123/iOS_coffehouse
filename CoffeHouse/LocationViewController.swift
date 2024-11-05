9bf46b0cbbd0b59f5e3f98790ef486422252e8ae
hoangkimduc2003 <hduc09032003@gmail.com>
2024-11-05 18:09:32 +0700

    fix search bar

--- CoffeHouse/LocationViewController.swift
+++ CoffeHouse/LocationViewController.swift
@@ -9,24 +9,26 @@ import UIKit
 
 class LocationViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpSearchBar()
 
-        // Do any additional setup after loading the view.
     }
 
 
     @IBAction func didTap(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func setUpSearchBar(){
+        if let textFieldInsideSearchBar = self.searchBar.value(forKey: "searchField") as? UITextField {
+            textFieldInsideSearchBar.backgroundColor = UIColor.white
+        }
+        searchBar.layer.cornerRadius = 28
+        searchBar.layer.masksToBounds = true
+        searchBar.layer.borderColor = UIColor(.black).cgColor
+        searchBar.layer.borderWidth = 1.0
     }
-    */
 
 }
