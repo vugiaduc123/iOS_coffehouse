31deb2ec7384b36884f57ee4f4f84149a765e83a
Duc <vugiaduc9962986@gmail.com>
2024-11-07 08:29:53 +0700

    main/layout/Model

---
+++ CoffeHouse/CoffeHouse/Services/Managers/ExampleManager.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleManager.swift
+//  CoffeHouse
+//
+//  Created by Vũ Đức on 7/11/24.
+//
+
+import Foundation
+import UIKit
+
+// MARK: We call Api Here in folder Managers
+class ExampleManager{
+    static var shared = ExampleManager()
+    
+    func getData(){
+        
+    }
+}
