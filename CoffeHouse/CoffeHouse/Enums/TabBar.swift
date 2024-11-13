96317b2334753dc2091d2375ebd47b15c4d56f0c
hoangkimduc2003 <hduc09032003@gmail.com>
2024-11-13 23:05:26 +0700

    fix color

--- CoffeHouse/CoffeHouse/Enums/TabBar.swift
+++ CoffeHouse/CoffeHouse/Enums/TabBar.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import UIKit
+
+enum Tabbar: String {
+    case home
+    case favorite
+    case cart
+    case profile
+
+    func icon(isSelected: Bool) -> UIImage? {
+        let iconName = "ic_\(self.rawValue)_\(isSelected ? "selected" : "unselected")"
+        return UIImage(named: iconName)
+    }
+
+    func name() -> String {
+        return self.rawValue.capitalized
+    }
+}
