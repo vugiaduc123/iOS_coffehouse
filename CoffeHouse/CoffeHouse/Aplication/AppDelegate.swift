2dd26897ffa88cc639732aae1a91a1f515d4c797
kattd2907 <datak53yma@gmail.com>
2024-11-15 13:02:59 +0700

    Reup Login and LS

--- CoffeHouse/CoffeHouse/Aplication/AppDelegate.swift
+++ CoffeHouse/CoffeHouse/Aplication/AppDelegate.swift
@@ -2,27 +2,27 @@
 //  AppDelegate.swift
 //  CoffeHouse
 //
-//  Created by Vũ Đức on 5/11/24.
+//  Created by Vũ Đức on 1/11/24.
 //
 
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow()
-        let view = CustomTabBarController()
-        let navigationVC = UINavigationController(rootViewController: view)
-        window?.rootViewController = navigationVC
+        
+        self.window = UIWindow()
+        let loginVC = LoginViewController()
+        let loginNavigationVC = UINavigationController(rootViewController: loginVC)
+        window?.rootViewController = loginNavigationVC
         window?.makeKeyAndVisible()
-        print("Register Layout")
+        
         return true
     }
-
-
+    
 
 
 }
--- CoffeHouse/CoffeHouse/UIs/Login&Register/LoginViewController.swift
+++ CoffeHouse/CoffeHouse/UIs/Login&Register/LoginViewController.swift
@@ -2,16 +2,90 @@
 //  LoginViewController.swift
 //  CoffeHouse
 //
-//  Created by Vũ Đức on 5/11/24.
+//  Created by Vũ Đức on 4/11/24.
 //
 
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    @IBOutlet weak var lbMissInfomation: UILabel!
+    private var users: [UserEntity] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        passwordText.isSecureTextEntry = true
+        lbMissInfomation.isHidden = true
+        
+    }
+    
+    
+    
+    @IBAction func btLogin(_ sender: Any) {
+        loadData()
+        guard let enterEmail = emailText.text,
+              let enterPassword = passwordText.text,
+              !enterEmail.isEmpty, !enterPassword.isEmpty else {
+            lbMissInfomation.isHidden = false
+            return
+        }
+        do {
+            var isLoginSuccessful = false
+                    for user in users {
+                        if user.email == enterEmail && user.password == enterPassword {
+                            isLoginSuccessful = true
+                            lbMissInfomation.isHidden = true
+                            UserDefaults.standard.set(true, forKey: "isActive")
+                            let homeVC = HomeViewController()
+                            self.navigationController?.pushViewController(homeVC, animated: true)
+                            break
+                        }
+                    }
+                    
+                    if !isLoginSuccessful {
+                        lbMissInfomation.isHidden = true
+                        wrongNotificationLogin()
+                    }
+            }
     }
 
+    @IBAction func btRegister(_ sender: Any) {
+        let registerVC = RegisterViewController()
+        self.navigationController?.pushViewController(registerVC, animated: true)
+    }
+    
+}
+
+// MARK Layout - Config
+extension LoginViewController{
+    
+    func wrongNotificationLogin() {
+        let wrongNotificationController = UIAlertController(title: "Wrong email or password", message: "", preferredStyle: .alert)
+        wrongNotificationController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(wrongNotificationController, animated: true, completion: nil)
+    }
+}
+
+
+// MARK Method
+extension LoginViewController{
+    
+}
+
+// MARK Load Jsonfile
+extension LoginViewController{
+    private func loadData(){
+        guard let fileURL = Bundle.main.url(forResource: "User", withExtension: "json") else {
+            print("File not found")
+            return
+        }
+        do {
+            let userData = try Data(contentsOf: fileURL)
+            self.users = try JSONDecoder().decode([UserEntity].self, from: userData)
+        } catch {
+            print(String(describing: error))
+        }
+    }
 }
