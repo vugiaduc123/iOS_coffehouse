1a0a7b0c0ead7da21dd67f213b1361040fdcdb60
Nguyen Quang Huy <nquanghuy.2003@gmail.com>
2024-11-15 23:33:26 +0700

    favourite item

---
+++ CoffeHouse/CoffeHouse/UIs/Favourite/FavouriteCell.swift
@@ -0,0 +1,68 @@
+//
+//  FavouriteCell.swift
+//  CoffeHouse
+//
+//  Created by Apple on 14/11/24.
+//
+
+import UIKit
+import SDWebImage
+
+class FavouriteCell: UITableViewCell {
+   
+    @IBOutlet weak var imageProduct: UIImageView!
+    
+    @IBOutlet weak var rate: UILabel!
+    @IBOutlet weak var btnAddToCart: UIButton!
+    @IBOutlet weak var btnFavourite: UIButton!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var backView: UIView!
+    
+    var favButtonPressed: (() -> ()) = {}
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+     
+        config()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func btnRemoveFavourite(_ sender: UIButton) {
+        favButtonPressed()
+    }
+        
+    func bind(with product: ProductEntity) {
+        name.text = product.name
+        price.text = String(product.price)
+        rate.text = String(product.rate)
+        imageProduct.sd_setImage(with: URL(string: product.fullPath), placeholderImage: UIImage(named: "placeholder.png"))
+    }
+    
+    func config() {
+        configImageProduct()
+        configCell()
+    }
+}
+
+
+//MARK: - Config
+extension FavouriteCell {
+    func configImageProduct() {
+        imageProduct.layer.cornerRadius = 14
+    }
+    
+    func configCell() {
+        self.backgroundColor = UIColor.clear
+        self.layer.borderColor = UIColor.lightGray.cgColor
+        self.layer.borderWidth = 1
+        self.layer.cornerRadius = 8
+        self.clipsToBounds = true
+    }
+}
