89a39b19980a5919a6d590b1bb43400eb20ef945
kattd2907 <datak53yma@gmail.com>
2024-11-16 16:54:16 +0700

    Profile and logout

---
+++ CoffeHouse/CoffeHouse/UIs/Home/ProfileViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ProfileViewController.swift
+//  CoffeHouse
+//
+//  Created by Macbook on 14/11/24.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    var loggedInUser: UserEntity?
+    
+    @IBOutlet weak var usernameLb: UILabel!
+    @IBOutlet weak var phonenumberLb: UILabel!
+    @IBOutlet weak var addressLb: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let userData = UserDefaults.standard.data(forKey: "loggedInUser"),
+                   let user = try? JSONDecoder().decode(UserEntity.self, from: userData) {
+                    self.loggedInUser = user
+                }
+        if let user = loggedInUser {
+            usernameLb.text = user.name
+            phonenumberLb.text = user.phone
+            addressLb.text = user.address
+                }
+    }
+    
+    
+    @IBAction func logOutBt(_ sender: Any) {
+        UserDefaults.standard.set(false, forKey: "isActive")
+        let loginVC = LoginViewController()
+        _ = UINavigationController(rootViewController: loginVC)
+            navigationController?.viewControllers = [loginVC]
+    }
+}
+
+
+
